87aac0f4c0e21ce8dab24afac7f1c8909ce77ed7
Alextadeledev <alex.tadele.dev@gmail.com>
2026-03-24 16:35:02 +0300

    final project implementation - all features completed

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from task_manager import add_task, view_tasks
+from task_manager import add_task, view_tasks,complete_task,delete_task
 
 def main():
     while True:
         print("\n1. Add Task")
-        print("2.Show Tasks")
-        print("3. Exit")
-
+        print("2.View All Tasks")
+        print("3. Complete Task")
+        print("4. Delete Task")
+        print("5. Exit")
         choice = input("Choose: ")
 
         if choice == "1":
@@ -16,10 +17,15 @@ def main():
             view_tasks()
 
         elif choice == "3":
-            break
+           task_id = input("Enter task ID to complete: ")
+           complete_task(task_id)
 
-        else:
-            print("Invalid choice")
+        elif choice == "4":
+            task_id = input("Enter task ID to delete: ")
+            delete_task(task_id)
+
+        elif choice == "5":
+               break
 
 if __name__ == "__main__":
     main()
--- task_manager.py
+++ task_manager.py
@@ -25,3 +25,26 @@ def view_tasks():
     for task in tasks:
         status = "✅" if task["completed"] else "❌"
         print(f'{task["id"]}. {task["title"]} [{status}]')
+        
+def complete_task(task_id):
+    tasks = load_tasks()
+
+    for task in tasks:
+        if task["id"] == int(task_id):
+            task["completed"] = True
+            save_tasks(tasks)
+            print("✅ Task marked as completed!")
+            return
+
+    print("❌ Task not found!")
+def delete_task(task_id):
+    tasks = load_tasks()
+
+    updated_tasks = [task for task in tasks if task["id"] != int(task_id)]
+
+    if len(tasks) == len(updated_tasks):
+        print("❌ Task not found!")
+        return
+
+    save_tasks(updated_tasks)
+    print("🗑️ Task deleted successfully!")
